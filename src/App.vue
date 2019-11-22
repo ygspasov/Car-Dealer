acdf6c5af83fe23631d75aa8d3e68109e9b6aafc
Yavor Spassov <ygspasov@gmail.com>
2019-11-22 11:46:37 +0200

    Setting up the innitial project and adding JSON data for cars.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
 </template>
 
+<script>
+</script>
 <style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
