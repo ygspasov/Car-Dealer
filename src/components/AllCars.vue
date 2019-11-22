d1b45a868853a94075e25f9b5cbe508a1e9c72dc
Yavor Spassov <ygspasov@gmail.com>
2019-11-22 15:47:21 +0200

    adding JSON server and serving data on the Front End with Axios

---
+++ src/components/AllCars.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="main">
+    <h1>All cars</h1>
+    {{ cars }}
+  </div>
+</template>
+
+<script>
+const axios = require("axios");
+export default {
+  data() {
+    return {
+      cars: []
+    };
+  },
+  created() {
+    this.loadCars();
+  },
+  methods: {
+    loadCars() {
+      axios
+        .get("http://localhost:3000/cars")
+        .then(response => {
+          this.cars = response.data;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+      return "";
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,11 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <div class="home"><AllCars /></div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import AllCars from "../components/AllCars.vue";
 export default {
   name: "home",
-  components: {
-    HelloWorld
-  }
+  components: { AllCars }
 };
 </script>
