2de85265e0fbd2158a670393fc1079d498622e9a
Yavor Spassov <ygspasov@gmail.com>
2019-12-16 15:26:09 +0200

    Adding a dropdown button with search options.

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,11 +1,40 @@
 <template>
-  <v-col>
-    <h1>Sidebar</h1>
-  </v-col>
+  <v-container id="button-dropdown">
+    <v-row>
+      <v-col cols="12">
+        <v-overflow-btn
+          class="my-2"
+          :items="SortBy"
+          label="Sort cars by"
+          target="#button-dropdown"
+          v-model="sortWord"
+          @input="handleClick(sortWord)"
+        ></v-overflow-btn>
+      </v-col>
+      {{sortWord}}
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      SortBy: [
+        "By model",
+        "By price ascending",
+        "By price descending",
+        "By manufacturer"
+      ],
+      sortWord: ""
+    };
+  },
+  methods: {
+    handleClick(sortWord) {
+      console.log("sortWord: " + sortWord);
+    }
+  }
+};
 </script>
 
 <style>
